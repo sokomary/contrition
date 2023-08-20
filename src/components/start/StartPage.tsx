8c24446dd03fc4529fd453abe682b3c77348beb6
sokomary <miss.sokolova-marya2014yandex.ru>
2023-08-20 18:45:32 +0200

    cn-9 add random recipes

--- src/components/start/StartPage.tsx
+++ src/components/start/StartPage.tsx
@@ -8,13 +8,29 @@ import i18next from '../../i18next';
 import { Button } from '../ui/Button';
 import { Container } from '../ui/Container';
 import { ReactComponent as LinkSvg } from '../../icons/link.svg';
+import { Tag } from '../../domain/Tag';
 
 const StartPage = () => {
   const [recipes, setRecipes] = useState<Recipe[]>([]);
+  const [randomRecipe, setRandomRecipe] = useState<Recipe | undefined>(undefined);
   const [open, setOpen] = useState(false);
+
+  const [tags, setTags] = useState<Tag[]>([]);
+  const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
+
+  const getTags = () => {
+    API.getTags().then((res) => setTags(res));
+  };
+  useEffect(() => {
+    getTags();
+  }, []);
 
   const getRecipes = () => {
     API.getRecipes().then((res) => setRecipes(res));
+  };
+
+  const getRandomRecipe = () => {
+    API.getRandomRecipe(selectedTags.map((t) => t.name)).then((res) => setRandomRecipe(res));
   };
 
   const deleteRecipe = (recipe: Recipe) => {
@@ -35,13 +51,31 @@ const StartPage = () => {
         }}
       />
 
+      {randomRecipe && (
+        <div style={{ color: 'red' }}>{randomRecipe.name}</div>
+      )}
+
       <Container vertical gap={50}>
         <Container style={{ justifyContent: 'flex-end' }}>
-          <Button
-            onClick={() => setOpen(true)}
-          >
+          <Button onClick={() => setOpen(true)}>
             {i18next.t('startpage:recipes.actions.add')}
           </Button>
+          <Container vertical gap={20}>
+            <Button onClick={getRandomRecipe}>
+              {i18next.t('startpage:recipes.actions.random')}
+            </Button>
+            {tags.map((t) => (
+              <TagName
+                bold={selectedTags.includes(t)}
+                key={t.id}
+                onClick={() => (selectedTags.includes(t)
+                  ? setSelectedTags([...selectedTags.filter((tag) => tag.id !== t.id)])
+                  : setSelectedTags([...selectedTags, t]))}
+              >
+                {t.name}
+              </TagName>
+            ))}
+          </Container>
         </Container>
 
         <StyledContainer gap={42}>
@@ -52,13 +86,14 @@ const StartPage = () => {
                   {r.name}
                   <Link style={{ height: 16, alignSelf: 'center' }} to={r.link}><LinkIcon /></Link>
                 </Header>
+                {r.tags.map((t) => (<>{t.name}</>))}
                 <Container gap={5}>
                   <Element bold>{r.calories.toFixed(2)}</Element>
                   <Element>{r.protein.toFixed(2)}</Element>
                   <Element>{r.fats.toFixed(2)}</Element>
                   <Element>{r.carbohydrates.toFixed(2)}</Element>
                 </Container>
-                <img src={r.img} alt="картинка" />
+                <Img src={r.img} />
               </Container>
               <Container style={{ justifyContent: 'flex-end' }}>
                 <Button onClick={() => deleteRecipe(r)}>{i18next.t('startpage:recipes.actions.delete')}</Button>
@@ -83,7 +118,7 @@ const StyledContainer = styled(Container)`
 const Card = styled.div`
   padding: 20px;
   width: 300px;
-  height: 400px;
+  height: 415px;
   border-radius: 20px;
   background-color: pink;
   
@@ -114,6 +149,23 @@ const Element = styled.div<{ bold?: boolean }>`
   color: #a4003f;
   font-weight: ${({ bold }) => (bold ? 'bold' : 'normal')};
 
+`;
+
+const Img = styled.div<{ src: string }>`
+  width: 260px;
+  height: 260px;
+  background-image: url(${({ src }) => src});
+  background-size: contain;
+  background-position: center;
+  background-repeat: no-repeat;
+
+  border-radius: 15px;
+`;
+
+const TagName = styled.div<{ bold: boolean }>`
+  color: pink;
+  font-weight: ${({ bold }) => (bold ? 'bold' : 'normal')};
+  cursor: pointer;
 `;
 
 export { StartPage };
