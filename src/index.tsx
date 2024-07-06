99b670c2ded12ce6119d1f0b58c87738ea19d839
sokomary <miss.sokolova-marya2014yandex.ru>
2024-07-06 10:00:25 +0200

    add github actions

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import 'src/index.css';
 import { CookiesProvider } from 'react-cookie';
 import App from 'src/App';
-import reportWebVitals from 'src/reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -17,8 +16,3 @@ root.render(
     </CookiesProvider>
   </div>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
