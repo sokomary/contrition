cf79680732d7891582cbe64cd6b8d5f199cf16f2
sokomary <miss.sokolova-marya2014yandex.ru>
2024-09-04 00:57:22 +0300

    cn-65 Move from webpack to vite

---
+++ vite.config.ts
@@ -0,0 +1,56 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+// import mkcert from 'vite-plugin-mkcert';
+import { VitePWA } from 'vite-plugin-pwa';
+
+export default defineConfig({
+    server: {
+      proxy: {
+        '/api': {
+          target: 'https://localhost:9443',
+          changeOrigin: true,
+          secure: false,
+        }
+      },
+    },
+    plugins: [
+      react({ include: '**/*.{jsx,tsx}' }),
+      // mkcert(),
+      VitePWA({
+        registerType: 'prompt',
+        manifest: {
+          "short_name": "Contrition",
+          "name": "Conscious nutrition",
+          "icons": [
+            {
+              "src": "logo64.png",
+              "sizes": "64x64 32x32 24x24 16x16",
+              "type": "image/x-icon"
+            },
+            {
+              "src": "logo192.png",
+              "type": "image/png",
+              "sizes": "192x192"
+            },
+            {
+              "src": "logo512.png",
+              "type": "image/png",
+              "sizes": "512x512"
+            }
+          ],
+          "start_url": ".",
+          "display": "standalone",
+          "theme_color": "#000000",
+          "background_color": "#ffffff"
+        },
+      }),
+    ],
+    resolve: {
+      alias: { src: path.resolve(__dirname, 'src') }
+    },
+    build: {
+      outDir: './build',
+    },
+    publicDir: './public',
+});
